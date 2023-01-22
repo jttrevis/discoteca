20cc03530c4d8ec4b8fa229c8b4d544cde50ae9a
jttrevis <jr.trevis@hotmail.com>
2023-01-22 17:02:07 +0000

    fixed photos link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,43 @@
 import React from 'react';
 import styles from './Footer.module.scss';
 export const Footer = () => {
-  return (
-    <div className='name'>
-      <footer className={styles.footerContainer}>
-        <a href="https://www.instagram.com/robertotrevisol/">
-          <img src="images/instagram.png" alt="instagram" />
-        </a>
-        <a href="https://www.facebook.com/roberto.trevisol">
-          <img src="images/facebook.png" alt="facebook" />
-        </a>
-        <a href="https://wa.me/+5545999462731">
-          <img src="images/whatsapp.png" alt="whatsapp" />
-        </a>
-      </footer>
-      <span >
-        <p >Created by Junior Trevisol</p>
-      </span>
-    </div>
-  )
-
-
-
+	return (
+		<div className='name'>
+			<footer className={styles.footerContainer}>
+				<a
+					target='_blank'
+					rel='noreferrer'
+					href='https://www.instagram.com/robertotrevisol/'
+				>
+					<img
+						src='images/instagram.png'
+						alt='instagram'
+					/>
+				</a>
+				<a
+					target='_blank'
+					rel='noreferrer'
+					href='https://www.facebook.com/roberto.trevisol'
+				>
+					<img
+						src='images/facebook.png'
+						alt='facebook'
+					/>
+				</a>
+				<a
+					target='_blank'
+					rel='noreferrer'
+					href='https://wa.me/+5545999462731'
+				>
+					<img
+						src='images/whatsapp.png'
+						alt='whatsapp'
+					/>
+				</a>
+			</footer>
+			<span>
+				<p>Created by Junior Trevisol</p>
+			</span>
+		</div>
+	);
 };
--- src/imagesData.js
+++ src/imagesData.js
@@ -1,24 +1,44 @@
 export const images = [
 	{
-		original: "https://scontent-man2-1.xx.fbcdn.net/v/t39.30808-6/312109812_781883859801855_6525208658030315624_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=fImvV5YNDYQAX9xoj81&_nc_ht=scontent-man2-1.xx&oh=00_AfDcPxa--Faaiumi6rcPzNDO8olJgJv_KkiElBrvwibVkA&oe=636C1539"
+		original: 'images/photos/1.jpg',
 	},
 	{
-		original: "https://scontent-man2-1.xx.fbcdn.net/v/t39.30808-6/312008893_781883833135191_9051202060427816910_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=auJx44ymvsMAX_MyAjW&_nc_ht=scontent-man2-1.xx&oh=00_AfBibwvAqXSbaHQtbT6lP7IvVrIoL68OsSgOTN1Q-1Nzuw&oe=636C8DF7"
+		original: 'images/photos/2.jpg',
 	},
 	{
-		original: "https://scontent-man2-1.xx.fbcdn.net/v/t39.30808-6/311949815_781883766468531_7835277295805559592_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=RMfV9dy6Qo8AX86LG29&_nc_oc=AQmESYDF2EaufHzt3F9tvat0TxGtigK0uDmTtf9Umm-FV-iROd65rmoOjoZadtGzGV8&_nc_ht=scontent-man2-1.xx&oh=00_AfAmOoEQOE4VkKIZtmVkycUY7Ytint0huoaT6Q7g_D1Zzw&oe=636C1775"
+		original: 'images/photos/3.jpg',
 	},
 
 	{
-		original: "https://scontent-man2-1.xx.fbcdn.net/v/t39.30808-6/308513976_781883453135229_5292061959075221737_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=972wFjSITAMAX8HLjaz&_nc_ht=scontent-man2-1.xx&oh=00_AfCFtTHdvbE931YBPzPOt0xjxGlogthobp_A3UbIk_bvEw&oe=636CF299"
+		original: 'images/photos/4.jpg',
 	},
 	{
-
-		original: "https://scontent-man2-1.xx.fbcdn.net/v/t39.30808-6/311778227_781883266468581_7515399304383576264_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=wKPfedlNihAAX9yMXoL&_nc_ht=scontent-man2-1.xx&oh=00_AfCyBmv9jtkunYYWxhUQgSvapMg1-bvfRoxjKKAfG5EsKQ&oe=636D5E02"
+		original: 'images/photos/5.jpg',
 	},
 	{
+		original: 'images/photos/6.jpg',
+	},
+	{
+		original: 'images/photos/7.jpg',
+	},
 
-		original: "https://scontent-man2-1.xx.fbcdn.net/v/t39.30808-6/307873770_781883203135254_1949882775630822313_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=ZkdqidLUiYkAX9rWM8a&_nc_ht=scontent-man2-1.xx&oh=00_AfBZnFofdiGT8TZS9mvxzK_0QR1Ltk_Ukc6CxvbwzgX8BQ&oe=636D6E72"
-	}
+	{
+		original: 'images/photos/8.jpg',
+	},
 
-]
+	{
+		original: 'images/photos/9.jpg',
+	},
+
+	{
+		original: 'images/photos/10.jpg',
+	},
+
+	{
+		original: 'images/photos/11.jpg',
+	},
+
+	{
+		original: 'images/photos/12.jpg',
+	},
+];
